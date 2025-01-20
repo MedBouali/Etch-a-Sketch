3d04b5c1b67d66ae9ea8f145538e3da11d3b5cea
Mohammed Bouali <68650187+MedBouali@users.noreply.github.com>
2025-01-20 19:31:41 +0100

    Add grid customization and hover effects
    - Created a mouseover event listener to change the color of divs on hover.
    - Added a button to prompt the user for the number of squares per side for a new grid.
    - Implemented an event listener for the button to trigger grid resizing.
    - Styled the button and hover effect for divs in style.css.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,31 @@
 const container = document.querySelector(".container");
 
-const SquaresPerSide = 50;
+function grid(squaresPerSide) {
+    const gridSize = squaresPerSide ** 2;
+    for(let i = 0 ; i < gridSize ; i++) {
+        const square = document.createElement("div");
+        square.classList.add("square");
+        square.style.height = 100 / squaresPerSide + "%";
+        square.style.width = 100 / squaresPerSide + "%";
+        container.appendChild(square);
 
-for(let i = 0 ; i < SquaresPerSide * SquaresPerSide ; i++) {
-    const grid = document.createElement("div");
-    grid.classList.add("grid");
-    grid.style.height = 100 / SquaresPerSide + "%";
-    grid.style.width = 100 / SquaresPerSide + "%";
-    container.appendChild(grid);
+        square.addEventListener("mouseover", () => {
+            square.classList.add("square-mouseover");
+        });
+    }
 }
+
+const button = document.querySelector(".btn");
+
+button.addEventListener("click", () => {
+    const numSquaresPerSide = parseInt(prompt("Enter the number of squares"));
+
+    if(numSquaresPerSide <= 100) {
+        container.innerHTML = "";
+        grid(numSquaresPerSide);
+    } else {
+        alert("Please enter a value less than 100.");
+    }
+});
+
+grid(16);
